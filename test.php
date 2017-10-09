2bba94589a606244caf6d095e049f9bd2bec1df4
Isaac Riceweber <brotchen@gmail.com>
2017-10-09 07:57:26 -0700

    Abandon PHP driven integration tests.

--- test.php
+++ test.php
@@ -1,4 +1,23 @@
 <?php
-$pattern = '/^\{(?:(.+?),)*\}$/';
-$result = preg_match($pattern, '{abc}', $matches);
-var_dump($result, $matches);
+
+require __DIR__ . '/vendor/autoload.php';
+
+$networkProc = new \Symfony\Component\Process\Process(<<<SH
+docker network create --internal minutes-integration
+SH
+);
+$networkProc->mustRun()->wait();
+$dbProc = new \Symfony\Component\Process\Process(<<<SH
+docker run \
+  --network=minutes-integration \
+  --network-alias=db \
+  --rm \
+  -e POSTGRES_USER=root \
+  -e POSTGRES_PASSWORD=secret \
+  -e POSTGRES_DB=minutes \
+  postgres:9-alpine
+SH
+);
+$dbProc->mustRun()->wait();
+
+
